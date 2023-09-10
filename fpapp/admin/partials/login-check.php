be175dafb7988786f3b51b51febc0836c9967316
Elijah <68749686+elijahladdie@users.noreply.github.com>
2023-09-10 20:29:47 +0300

    Delete fpapp directory

--- fpapp/admin/partials/login-check.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!isset($_SESSION['signed_in'])){
-    $_SESSION["not_found"] = "<p class='text-danger text-center'>Please Log in to Access the system</p>";
-    header("location:".URL."admin/portal/index.php");
-}
-
-?>
